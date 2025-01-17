8efe457d52325bdfee5b541a48b67b75cea96944
Karina Chistyh <karina.komarovich@gmail.com>
2025-01-17 13:08:39 +0100

    Add delete buttons
    Also some styles was added to improve layout

--- __tests__/HomeScreen-test.tsx
+++ __tests__/HomeScreen-test.tsx
@@ -6,6 +6,13 @@ import { graphql } from "msw";
 import { graphql as executeGraphQL } from "graphql";
 import { customRender, mockedSchema } from "@/src/mocks/urqlProvider";
 
+const defaultPosts = {
+  data: [
+    { id: 1, title: "title" },
+    { id: 2, title: "title" },
+  ],
+};
+
 describe("<HomeScreen />", () => {
   test("Post title renders correctly on HomeScreen", async () => {
     const { getByText } = customRender(<HomeScreen />);
@@ -47,5 +54,24 @@ describe("<HomeScreen />", () => {
     );
 
     await waitFor(() => getByText("awesome title 2"));
+  });
+
+  test("displays delete buttons for every post", async () => {
+    const { getAllByText } = customRender(<HomeScreen />);
+
+    server.use(
+      graphql.query("PostsQuery", async ({ query, variables }) => {
+        const { errors, data } = await executeGraphQL({
+          schema: mockedSchema,
+          source: query,
+          variableValues: variables,
+          rootValue: { posts: defaultPosts },
+        });
+
+        return HttpResponse.json({ errors, data });
+      })
+    );
+
+    await waitFor(() => expect(getAllByText("Remove")).toHaveLength(2));
   });
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,12 @@
 import { PropsWithChildren, useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { gql, useQuery } from "urql";
 
 export const CustomText = ({ children }: PropsWithChildren) => (
@@ -42,10 +49,37 @@ export default function HomeScreen() {
     );
 
   return (
-    <View>
-      {data.posts.data.splice(0, 10).map((post) => (
-        <Text key={post.id}>{post.title}</Text>
-      ))}
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={data.posts.data}
+        renderItem={({ item: post }) => {
+          return (
+            <View key={post.id} style={styles.post}>
+              <Text>{post.id}</Text>
+              <Text style={styles.postTitle}>{post.title}</Text>
+              <Button color="red" title="Remove" />
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  post: {
+    paddingHorizontal: 8,
+    paddingVertical: 2,
+    margin: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    backgroundColor: "#eee",
+    borderColor: "grey",
+    borderRadius: 5,
+    gap: 8,
+  },
+  postTitle: {
+    flex: 1,
+  },
+});
